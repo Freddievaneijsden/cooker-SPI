e9a0af5c9ead54bc4f6baff5e346124ad28cbaa4
Freddie Van Eijsden <freddievaneijsden@gmail.com>
2025-04-25 11:53:20 +0200

    add retention and target to annotation

--- Consumer/src/main/java/com/example/consumer/Main.java
+++ Consumer/src/main/java/com/example/consumer/Main.java
@@ -1,6 +1,7 @@
 package com.example.consumer;
 
 import com.example.cooker.Cooker;
+import com.example.cooker.Cuisine;
 
 import java.util.ServiceLoader;
 
@@ -14,5 +15,18 @@ public class Main {
                 .filter(cookerProvider -> cookerProvider.type().isAnnotation())
                 .map(ServiceLoader.Provider::get)
                 .toList();
+
+        for (Cooker dish : dishes) {
+            if (dishes.getClass().getSimpleName().startsWith("Swedish"))
+                dish.cook();
+        }
+
+        for (Cooker dish : serviceLoader) {
+            var annotation = dish.getClass().getAnnotation(Cuisine.class);
+            if (annotation == null)
+                System.out.println("No annotation of type Cuisine found");
+            else
+                System.out.println("Annotation Cuisine with value: " + annotation.value());
+        }
     }
 }
